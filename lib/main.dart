190c33d1a965566f33a58fe56daa175023cc0559
Danah <danah.kim@kakao.com>
2023-07-11 23:31:09 +0900

    fix: Full screen splash 이후 status bar 보이도록 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(
+    SystemUiMode.manual,
+    overlays: [SystemUiOverlay.bottom, SystemUiOverlay.top],
+  );
+
   runApp(const MyApp());
 }
 
